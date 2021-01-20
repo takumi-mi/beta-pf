8e422458ce694e63e0962f02c93a6ef28da7c856
takumi-mi <70736686+takumi-mi@users.noreply.github.com>
2021-01-20 23:35:57 +0900

    c

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import "./_assets/normalize.scss";
 import "./_assets/skeleton.scss";
 import "./_assets/styles.scss";
 
-// import App from "./App";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   rootElement
 );
