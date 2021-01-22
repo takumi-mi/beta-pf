6fa0482a3084ed6f8bc91e24e6ad1c72f4afdfc1
takumi-mi <70736686+takumi-mi@users.noreply.github.com>
2021-01-23 03:50:46 +0900

    更新

--- src/article/tecblog/react/List.js
+++ src/article/tecblog/react/List.js
@@ -3,29 +3,31 @@ import { Link } from "react-scroll";
 
 export default function List() {
   return (
-    <div class="row">
-      <Link
-        activeClass="active"
-        to="HowToStart"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={800}
-        className="one-half button column"
-      >
-        Reactの導入
-      </Link>
-      <Link
-        activeClass="active"
-        to="HowToSass"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={800}
-        className="one-half button column"
-      >
-        Reactでsass(scss)を使う方法
-      </Link>
+    <div>
+      <div class="row">
+        <Link
+          activeClass="active"
+          to="HowToStart"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={800}
+          className="one-half button column"
+        >
+          Reactの導入
+        </Link>
+        <Link
+          activeClass="active"
+          to="HowToSass"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={800}
+          className="one-half button column"
+        >
+          Reactでsass(scss)を使う方法
+        </Link>
+      </div>
     </div>
   );
 }
