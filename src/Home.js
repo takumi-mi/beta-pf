33ba58ce844894dd78dab1e4c1c04a0bd3e9d717
takumi-mi <70736686+takumi-mi@users.noreply.github.com>
2021-01-28 15:30:55 +0900

    更新

--- src/Home.js
+++ src/Home.js
@@ -6,18 +6,20 @@ export default function Home() {
   return (
     <div className="Home">
       <article data-aos="fade" data-aos-duration="1000">
-        <h2 className="HomeTitle">Thoughts in Open.β</h2>
-        <p>"Open.β"とは試験的、試作版を意味する言葉。</p>
-        <p>
-          技術と表現の新しい可能性を探求することに終わりは無く
-          <br />
-          常に進化をし続けていくことを目的とする。
-        </p>
-        <p>
-          また、自身の研究や開発している内容は
-          <br />
-          全てオープンソースとして発表する。
-        </p>
+        <div className="Thoughts">
+          <h2 className="HomeTitle">Thoughts in Open.β</h2>
+          <p>"Open.β"とは試験的、試作版を意味する言葉。</p>
+          <p>
+            技術と表現の新しい可能性を探求することに終わりは無くう
+            <br />
+            常に進化をし続けていくことを目的とする。
+          </p>
+          <p>
+            また、自身の研究や開発している内容は
+            <br />
+            全てオープンソースとして発表する。
+          </p>
+        </div>
         <hr />
         <h2 data-aos="fade" data-aos-duration="1000">
           Works
